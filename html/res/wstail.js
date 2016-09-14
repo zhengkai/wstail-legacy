692c6e4d448220db0970b2df3a4d4829291eb35f
Zheng Kai <zhengkai@gmail.com>
2016-09-14 17:11:08 +0800

    up html

--- html/res/wstail.js
+++ html/res/wstail.js
@@ -7,11 +7,25 @@ var WsTail = {
 	ver: 0,
 	connectionId: 0,
 	wait: 1,
-	create: function(url, file, oTarget) {
+	create: function(option) {
  		var o = $.extend(true, {}, WsTail);
-		o.target = oTarget;
-		o.file = file;
-		o.url = url;
+		o.target = option.target || false;
+		if (!o.target) {
+			this._errorLog('no target', option);
+			return;
+		}
+		o.file = option.file || false;
+		if (!o.file) {
+			this._errorLog('no file', option);
+			return;
+		}
+		o.url = option.ws || false;
+		if (!o.url) {
+			this._errorLog('no ws', option);
+			return;
+		}
+		console.log(option)
+		o.parse  = option.parse || false;
  		o.init();
 		return o;
 	},
@@ -26,7 +40,6 @@ var WsTail = {
 		ws.onclose   = function (e) { $this._wsClose(e) }
 		ws.onmessage = function (e) { $this._wsMessage(e) }
 		ws.onerror   = function (e) { $this._wsError(e) }
-
 	},
 	_wsOpen: function(e) {
 		this.wait = 1;
@@ -34,6 +47,7 @@ var WsTail = {
 	_wsClose: function(e) {
 		var $this =  this
 		setTimeout(function () {
+			$this.ws = null;
 			$this.init();
 		}, this.wait * 1000);
 		if (this.wait < 4) {
@@ -51,8 +65,12 @@ var WsTail = {
 
 		if (cmd == '>') {
 			var oText = $('<span></span>');
-			oText.text(s.substring(1));
-			this.offset += (s.length - 1);
+			s = s.substring(1);
+			this.offset += this._utf8length(s);
+			if (this.parse) {
+				s = this.parse(s)
+			}
+			oText.html(s);
 			this.target.append(oText);
 			this.target.scrollTop(this.target[0].scrollHeight);
 			return;
@@ -73,5 +91,12 @@ var WsTail = {
 		}
 	},
 	_wsError: function(e) {
+	},
+	_utf8length: function(str) {
+		var m = encodeURIComponent(str).match(/%[89ABab]/g);
+		return str.length + (m ? m.length : 0);
+	},
+	_errorLog: function(s, o) {
+		console.log('WsTail Error: ' + s, "\n", o);
 	}
 }
